483eaa19a04229fac867d25a318cb8d8ec302802
Isabel Molina <mariaisabelmolinafer@gmail.com>
2023-01-13 07:41:14 -0300

    agrego jsx y classname

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as footer } from './footer';
+export { default as cart } from './cart';
+export { default as FooterBanner } from './FooterBanner';
+export { default as HeroBanner } from './HeroBanner';
+export { default as layout } from './layout';
+export { default as navbar } from './navbar';
+export { default as product } from './product';
